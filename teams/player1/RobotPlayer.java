00ef7cac7edc43a9031b648dd3a8a039f0bd1b3a
Zack Youngren <zack@youngren.io>
2015-01-22 13:12:22 -0500

    beavers spawn and move

--- teams/player1/RobotPlayer.java
+++ teams/player1/RobotPlayer.java
@@ -1,11 +1,23 @@
 package player1;
-
 import battlecode.common.*;
 
 public class RobotPlayer {
-
   public static void run(RobotController rc) {
-
+    while (true) {
+      try {
+        if (rc.getType() == RobotType.HQ) {
+          if (rc.isCoreReady() && rc.canSpawn(Direction.NORTH, RobotType.BEAVER)) {
+            rc.spawn(Direction.NORTH, RobotType.BEAVER);
+          }
+        } else if (rc.getType() == RobotType.BEAVER) {
+          if (rc.isCoreReady() && rc.canMove(Direction.NORTH)) {
+            rc.move(Direction.NORTH);
+          }
+        }
+      } catch (GameActionException e) {
+        e.printStackTrace();
+      }
+      rc.yield();
+    }
   }
-
 }
